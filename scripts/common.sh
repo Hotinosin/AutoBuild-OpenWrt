d4951c66f3613720c59387f782664d3c88a2d6b0
Hotinosin <87741985+Hotinosin@users.noreply.github.com>
2025-03-26 20:16:47 +0800

    Update common.sh

--- scripts/common.sh
+++ scripts/common.sh
@@ -43,15 +43,15 @@ rm_package "miniupnpd"
 # 添加package
 git clone -q --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 git clone -q --depth=1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone -q --depth=1 https://github.com/sbwml/luci-app-alist.git package/alist
-git clone -q --depth=1 https://github.com/sbwml/luci-app-mosdns.git package/mosdns
-git clone -q --depth=1 https://github.com/sbwml/v2ray-geodata.git package/v2ray-geodata
+#git clone -q --depth=1 https://github.com/sbwml/luci-app-alist.git package/alist
+#git clone -q --depth=1 https://github.com/sbwml/luci-app-mosdns.git package/mosdns
+#git clone -q --depth=1 https://github.com/sbwml/v2ray-geodata.git package/v2ray-geodata
 git clone -q --depth=1 https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
 git clone -q --depth=1 https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
 git clone -q --depth=1 https://github.com/sirpdboy/luci-app-taskplan.git package/luci-app-taskplan
 git clone -q --depth=1 https://github.com/sirpdboy/netspeedtest.git package/luci-app-netspeedtest
-git clone -q --depth=1 https://github.com/tty228/luci-app-wechatpush.git package/luci-app-wechatpush
-git clone -q --depth=1 https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
+#git clone -q --depth=1 https://github.com/tty228/luci-app-wechatpush.git package/luci-app-wechatpush
+#git clone -q --depth=1 https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
 
 git_sparse_clone() {
     branch="$1" repourl="$2" repodir="$3"
@@ -65,22 +65,22 @@ git_sparse_clone() {
 
 git_sparse_clone main https://github.com/kiddin9/kwrt-packages.git luci-app-control-timewol
 git_sparse_clone main https://github.com/kiddin9/kwrt-packages.git luci-app-onliner
-git_sparse_clone main https://github.com/linkease/nas-packages-luci.git luci/luci-app-ddnsto
-git_sparse_clone main https://github.com/ophub/luci-app-amlogic.git luci-app-amlogic
-git_sparse_clone master https://github.com/linkease/nas-packages.git network/services/ddnsto
-git_sparse_clone master https://github.com/lisaac/luci-app-dockerman.git applications/luci-app-dockerman
-git_sparse_clone master https://github.com/vernesong/OpenClash.git luci-app-openclash
+#git_sparse_clone main https://github.com/linkease/nas-packages-luci.git luci/luci-app-ddnsto
+#git_sparse_clone main https://github.com/ophub/luci-app-amlogic.git luci-app-amlogic
+#git_sparse_clone master https://github.com/linkease/nas-packages.git network/services/ddnsto
+#git_sparse_clone master https://github.com/lisaac/luci-app-dockerman.git applications/luci-app-dockerman
+#git_sparse_clone master https://github.com/vernesong/OpenClash.git luci-app-openclash
 
 git_sparse_clone master https://github.com/immortalwrt/luci.git applications/luci-app-ddns-go
 git_sparse_clone master https://github.com/immortalwrt/luci.git applications/luci-app-minidlna
-git_sparse_clone master https://github.com/immortalwrt/luci.git applications/luci-app-smartdns
-git_sparse_clone master https://github.com/immortalwrt/luci.git applications/luci-app-sqm
+#git_sparse_clone master https://github.com/immortalwrt/luci.git applications/luci-app-smartdns
+#git_sparse_clone master https://github.com/immortalwrt/luci.git applications/luci-app-sqm
 git_sparse_clone master https://github.com/immortalwrt/packages.git multimedia/minidlna
 git_sparse_clone master https://github.com/immortalwrt/packages.git net/ddns-go
 git_sparse_clone master https://github.com/immortalwrt/packages.git net/miniupnpc
 git_sparse_clone master https://github.com/immortalwrt/packages.git net/miniupnpd
-git_sparse_clone master https://github.com/immortalwrt/packages.git net/smartdns
-git_sparse_clone master https://github.com/immortalwrt/packages.git net/sqm-scripts
+#git_sparse_clone master https://github.com/immortalwrt/packages.git net/smartdns
+#git_sparse_clone master https://github.com/immortalwrt/packages.git net/sqm-scripts
 
 # requires golang latest version
 rm -rf feeds/packages/lang/golang
@@ -90,14 +90,14 @@ git clone -q --depth=1 https://github.com/sbwml/packages_lang_golang.git feeds/p
 cp -f ${GITHUB_WORKSPACE}/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 
 # samba解除root限制
-sed -i 's/invalid users = root/#&/g' feeds/packages/net/samba4/files/smb.conf.template
+#sed -i 's/invalid users = root/#&/g' feeds/packages/net/samba4/files/smb.conf.template
 
 # ttyd自动登录
 sed -i "s?/bin/login?/usr/libexec/login.sh?g" feeds/packages/utils/ttyd/files/ttyd.config
 
 # amlogic
-sed -i "s|amlogic_firmware_repo.*|amlogic_firmware_repo 'https://github.com/v8040/AutoBuild-OpenWrt'|g" package/luci-app-amlogic/root/etc/config/amlogic
-sed -i "s|amlogic_kernel_path.*|amlogic_kernel_path 'https://github.com/ophub/kernel'|g" package/luci-app-amlogic/root/etc/config/amlogic
+#sed -i "s|amlogic_firmware_repo.*|amlogic_firmware_repo 'https://github.com/v8040/AutoBuild-OpenWrt'|g" package/luci-app-amlogic/root/etc/config/amlogic
+#sed -i "s|amlogic_kernel_path.*|amlogic_kernel_path 'https://github.com/ophub/kernel'|g" package/luci-app-amlogic/root/etc/config/amlogic
 
 # 修改makefile
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/luci.mk/$(TOPDIR)\/feeds\/luci\/luci.mk/g' {}
@@ -131,12 +131,12 @@ replace_text() {
 
 replace_text "Argon 主题设置" "主题设置"
 replace_text "DDNS-Go" "DDNSGO"
-replace_text "DDNSTO 远程控制" "DDNSTO"
+#replace_text "DDNSTO 远程控制" "DDNSTO"
 replace_text "KMS 服务器" "KMS激活"
-replace_text "QoS Nftables 版" "QoS管理"
-replace_text "SQM 队列管理" "SQM管理"
-replace_text "动态 DNS" "动态DNS"
-replace_text "网络存储" "NAS"
-replace_text "解除网易云音乐播放限制" "音乐解锁"
+#replace_text "QoS Nftables 版" "QoS管理"
+#replace_text "SQM 队列管理" "SQM管理"
+#replace_text "动态 DNS" "动态DNS"
+#replace_text "网络存储" "NAS"
+#replace_text "解除网易云音乐播放限制" "音乐解锁"
 
 echo -e "\e[32m$0 [DONE]\e[0m"
